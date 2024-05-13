fd15b4648476271520b7e0c8bc44d40b75ecc244
Lxnthz <byanfidry@gmail.com>
2024-05-13 09:29:40 +0700

    welcome hampir jadi

---
+++ resources/js/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+    return <footer>
+        <div className="font-sans items-center h-20 flex justify-center w-screen text-center bg-gray-800 text-white 
+                        ">
+            © 2024. VoteLinker IPB University. All Rights Reserved.
+        </div>
+    </footer>
+}
+
+export default Footer;
